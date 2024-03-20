1d63498900768750ed8b880bbe82d83dd410b6c7
snoluv45 <pasitza115@gmail.com>
2024-03-21 01:48:42 +0700

    rrr

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React Test</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
           <p>Hello world Vite + React</p>
           <p>First used React + Vite</p>
-
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
